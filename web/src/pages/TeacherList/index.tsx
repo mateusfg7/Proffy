38a2ce565ed3679981502be5292f886a885d9cde
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-03 14:20:08 -0300

    update TeacherList page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 
-import "./styles.css";
 import PageHeader from "../../components/PageHeader";
+
+import "./styles.css";
 
 function TeacherList() {
   return (
     <div id='page-teacher-list' className='container'>
-      <PageHeader />
+      <PageHeader title='Estes são os proffys disponíveis.'>
+        <form id='search-teacher'>
+          <div className='input-block'>
+            <label htmlFor='subject'>Matéria</label>
+            <input type='text' id='subject' />
+          </div>
+
+          <div className='input-block'>
+            <label htmlFor='week_day'>Dia da semana</label>
+            <input type='text' id='week_day' />
+          </div>
+
+          <div className='input-block'>
+            <label htmlFor='time'>Hora</label>
+            <input type='text' id='time' />
+          </div>
+        </form>
+      </PageHeader>
     </div>
   );
 }
