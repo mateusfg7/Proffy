b476d2cf36b626db40729662c385bfbf71a280bf
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-03 13:11:54 -0300

    use Link component on Landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 import logoImg from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
 
@@ -25,15 +27,15 @@ function Landing() {
         />
 
         <div className='buttons-container'>
-          <a href='' className='study'>
+          <Link to='/study' className='study'>
             <img src={studyIcon} alt='Estudar' />
             Estudar
-          </a>
+          </Link>
 
-          <a href='' className='give-classes'>
+          <Link to='/give-classes' className='give-classes'>
             <img src={giveClassesIcon} alt='Estudar' />
             Dar Aulas
-          </a>
+          </Link>
         </div>
 
         <span className='total-connections'>
