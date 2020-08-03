9559b6c5ef45ade6bf840feccb9632b82a19d565
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-03 15:41:52 -0300

    import PageHeader on TecherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import PageHeader from "../../components/PageHeader";
+
 function TeacherForm() {
-  return <h1>teacher form</h1>;
+  return (
+    <div id='page-teacher-form' className='container'>
+      <PageHeader title='Que incrível que você quer dar aulas.' />
+    </div>
+  );
 }
 
 export default TeacherForm;
