b379bddb3782b802adbdf56c9f82014798a63274
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-04 14:44:22 -0300

    fix misterious error

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -87,6 +87,8 @@ export default class ClassesController {
 
       return response.status(201).send();
     } catch (err) {
+      console.log(err);
+
       await trx.rollback();
 
       return response.status(400).json({
