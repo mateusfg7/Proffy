e85e7988634bb9163cc7e78cdbdb2b8d2fa1ed31
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-04 12:51:22 -0300

    initial server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,11 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json({ Message: "hello Word" });
+});
 
 app.listen(3333);
