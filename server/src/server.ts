985485c6220222220213d6e63d844c0063307795
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-04 13:51:12 -0300

    refactor routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
-import express, { response } from "express";
+import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (request, response) => {
-  return response.json({ Message: "hello Word" });
-});
+app.use(routes);
 
 app.listen(3333);
