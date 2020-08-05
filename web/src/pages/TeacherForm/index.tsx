ce809e0ce4650e99bb98c92f74bc0f73fef94945
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 15:05:27 -0300

    add new fieldset on TeacherForm page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
@@ -10,6 +10,16 @@ import warningIcon from "../../assets/images/icons/warning.svg";
 import "./styles.css";
 
 function TeacherForm() {
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: 0, from: "", to: "" },
+  ]);
+
+  function addNewScheduleItem() {
+    setScheduleItems([...scheduleItems, { week_day: 0, from: "", to: "" }]);
+
+    scheduleItems.push();
+  }
+
   return (
     <div id='page-teacher-form' className='container'>
       <PageHeader
@@ -48,6 +58,38 @@ function TeacherForm() {
           />
 
           <Input name='cost' label='Custo da sua hora por aula' />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type='button' onClick={addNewScheduleItem}>
+              + Novo Horário
+            </button>
+          </legend>
+
+          {scheduleItems.map((scheduleItem) => {
+            return (
+              <div key={scheduleItem.week_day} className='schedule-item'>
+                <Select
+                  name='week_day'
+                  label='Dia da semana'
+                  options={[
+                    { value: "0", label: "Domingo" },
+                    { value: "1", label: "Segunda-feira" },
+                    { value: "2", label: "Terça-feira" },
+                    { value: "3", label: "Quarta-feira" },
+                    { value: "4", label: "Quinta-feira" },
+                    { value: "5", label: "Sexta-feira" },
+                    { value: "6", label: "Sábado" },
+                  ]}
+                />
+
+                <Input name='from' label='Das' type='time' />
+                <Input name='to' label='Até' type='time' />
+              </div>
+            );
+          })}
         </fieldset>
 
         <footer>
