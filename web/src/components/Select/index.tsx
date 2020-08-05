3d9fd0c6bf73139501a604306c61e3cb9ebed0a7
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 14:15:48 -0300

    update Select component

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -5,13 +5,29 @@ import "./styles.css";
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   name: string;
   label: string;
+  options: Array<{
+    value: string;
+    label: string;
+  }>;
 }
 
-const Select: React.FC<SelectProps> = ({ label, name, ...rest }) => {
+const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
   return (
     <div className='select-block'>
       <label htmlFor={name}>{label}</label>
-      <select id={name} {...rest} />
+      <select id={name} {...rest}>
+        <option value='' disabled selected hidden>
+          Selecione uma opção
+        </option>
+
+        {options.map((option) => {
+          return (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          );
+        })}
+      </select>
     </div>
   );
 };
