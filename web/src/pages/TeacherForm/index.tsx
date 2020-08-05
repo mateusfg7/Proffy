5adb1398421a191af8efe346389b4046ffa00188
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 13:53:08 -0300

    use Textarea on TeacherForm component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
+import Textarea from "../../components/TextArea";
 
 import warningIcon from "../../assets/images/icons/warning.svg";
 
@@ -22,6 +23,7 @@ function TeacherForm() {
           <Input name='name' label='Nome completo' />
           <Input name='avatar' label='Avatar' />
           <Input name='whatsapp' label='WhatsApp' />
+          <Textarea name='bio' label='Biografia' />
         </fieldset>
 
         <fieldset>
