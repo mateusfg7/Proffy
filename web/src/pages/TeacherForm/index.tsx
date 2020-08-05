2b4d380ebf91f34fb3469e67ece602cc4e58437e
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 10:50:21 -0300

    add description on header of TeacherForm page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,7 +5,10 @@ import PageHeader from "../../components/PageHeader";
 function TeacherForm() {
   return (
     <div id='page-teacher-form' className='container'>
-      <PageHeader title='Que incrível que você quer dar aulas.' />
+      <PageHeader
+        title='Que incrível que você quer dar aulas.'
+        description='o primeiro passo é preencher esse formulário de inscrição.'
+      />
     </div>
   );
 }
