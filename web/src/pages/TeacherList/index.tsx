b2b28f201aada3967a18668d1f4aaa49b9fd776e
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 14:23:57 -0300

    add select to 'Materia' and 'Dia da semana' on TeacherList page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,17 +2,45 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 import "./styles.css";
-import Input from "../../components/Input";
 
 function TeacherList() {
   return (
     <div id='page-teacher-list' className='container'>
       <PageHeader title='Estes são os proffys disponíveis.'>
         <form id='search-teachers'>
-          <Input name='subject' label='Matéria' />
-          <Input name='week_day' label='Dia da semana' />
+          <Select
+            name='subject'
+            label='Matéria'
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Ed. Física", label: "Ed. Física" },
+              { value: "Física", label: "Física" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Português", label: "Português" },
+              { value: "Quimica", label: "Quimica" },
+            ]}
+          />
+          <Select
+            name='week_day'
+            label='Dia da semana'
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-feira" },
+              { value: "2", label: "Terça-feira" },
+              { value: "3", label: "Quarta-feira" },
+              { value: "4", label: "Quinta-feira" },
+              { value: "5", label: "Sexta-feira" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
           <Input type='time' name='time' label='Hora' />
         </form>
       </PageHeader>
