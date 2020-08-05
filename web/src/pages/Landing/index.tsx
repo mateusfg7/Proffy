5b0d3171f682c95736be558b5e15886add63fc27
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 15:23:53 -0300

    use api on Landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -9,9 +9,21 @@ import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
+import api from "../../services/api";
+
 import "./styles.css";
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("connections").then((response) => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id='page-landing'>
       <div id='page-landing-content' className='container'>
@@ -39,7 +51,8 @@ function Landing() {
         </div>
 
         <span className='total-connections'>
-          Total de 200 conexões <img src={purpleHeartIcon} alt='Coração roxo' />
+          Total de {totalConnections} conexões{" "}
+          <img src={purpleHeartIcon} alt='Coração roxo' />
         </span>
       </div>
     </div>
