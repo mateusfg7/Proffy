f28c7babe9332afbd7b6b8d4e1203d1d079fd40d
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 13:20:23 -0300

    create footer for Teacherform page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
+
+import warningIcon from "../../assets/images/icons/warning.svg";
 
 import "./styles.css";
-import Input from "../../components/Input";
 
 function TeacherForm() {
   return (
@@ -28,6 +30,15 @@ function TeacherForm() {
           <Input name='subject' label='Matéria' />
           <Input name='cost' label='Custo da sua hora por aula' />
         </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt='Aviso importante' />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type='button'>Salvar cadastro</button>
+        </footer>
       </main>
     </div>
   );
