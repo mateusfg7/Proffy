503be89db078231f662f5ae16fc0df8038800884
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 13:13:02 -0300

    add styles on TeacherForm component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 
+import "./styles.css";
+
 function TeacherForm() {
   return (
     <div id='page-teacher-form' className='container'>
