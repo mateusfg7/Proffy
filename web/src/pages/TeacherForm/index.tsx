412f5f9e041384060de2ca0b94889799029c3496
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 10:57:44 -0300

    add new form fieldset on TeacherForm page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -9,6 +9,27 @@ function TeacherForm() {
         title='Que incrível que você quer dar aulas.'
         description='o primeiro passo é preencher esse formulário de inscrição.'
       />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <div className='input-block'>
+            <label htmlFor='name'>Nome completo</label>
+            <input type='text' id='name' />
+          </div>
+
+          <div className='input-block'>
+            <label htmlFor='avatar'>Avatar</label>
+            <input type='text' id='avatar' />
+          </div>
+
+          <div className='input-block'>
+            <label htmlFor='whatsapp'>WhatsApp</label>
+            <input type='text' id='whatsapp' />
+          </div>
+        </fieldset>
+      </main>
     </div>
   );
 }
