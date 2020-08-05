3860206b7be4ab77227ec1b5c28ef498f853a97e
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 17:17:27 -0300

    update TeacherItem component

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,43 +1,60 @@
 import React from "react";
 
+import api from "../../services/api";
+
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className='teacher-item'>
       <header>
-        <img
-          src='https://avatars1.githubusercontent.com/u/40613276?v=4'
-          alt='Mateus Felipe'
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Mateus Felipe</strong>
-          <span>Blockchain</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta de tecnologias descentralizadas.
-        <br />
-        <br />
-        Adora espalhar ideias de descentralisação de tudo que vier na frente,
-        principalmente por meio da tecnologia!
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>BTC 0.016</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type='button'>
+        <a
+          target='_blank'
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt='Whatsapp' />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
