355460a9b8c9aaf9ccb9d3d9cc37ea42d5ebf4e6
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 13:10:10 -0300

    fix Input component

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -10,7 +10,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 const Input: React.FC<InputProps> = ({ label, name, ...rest }) => {
   return (
     <div className='input-block'>
-      <label htmlFor={name}>Matéria</label>
+      <label htmlFor={name}>{label}</label>
       <input type='text' id={name} {...rest} />
     </div>
   );
