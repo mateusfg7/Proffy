6272fcb535f388a6918924fb342c99aa443cafd6
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 13:07:32 -0300

    refactor inputs using Input component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,26 +4,16 @@ import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
 
 import "./styles.css";
+import Input from "../../components/Input";
 
 function TeacherList() {
   return (
     <div id='page-teacher-list' className='container'>
       <PageHeader title='Estes são os proffys disponíveis.'>
         <form id='search-teachers'>
-          <div className='input-block'>
-            <label htmlFor='subject'>Matéria</label>
-            <input type='text' id='subject' />
-          </div>
-
-          <div className='input-block'>
-            <label htmlFor='week_day'>Dia da semana</label>
-            <input type='text' id='week_day' />
-          </div>
-
-          <div className='input-block'>
-            <label htmlFor='time'>Hora</label>
-            <input type='text' id='time' />
-          </div>
+          <Input name='subject' label='Matéria' />
+          <Input name='week_day' label='Dia da semana' />
+          <Input name='time' label='Hora' />
         </form>
       </PageHeader>
 
