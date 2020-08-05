217ab17c0763216a8baf212f0e445f6b5b16c61b
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-05 14:50:44 -0300

    update Select component

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -15,8 +15,8 @@ const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
   return (
     <div className='select-block'>
       <label htmlFor={name}>{label}</label>
-      <select id={name} {...rest}>
-        <option value='' disabled selected hidden>
+      <select defaultValue='' id={name} {...rest}>
+        <option value='' disabled hidden>
           Selecione uma opção
         </option>
 
