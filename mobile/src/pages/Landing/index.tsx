f0975d1a95121a751170327dbf28f269fd5cbb4e
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-06 11:30:25 -0300

    finish Landing page

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,8 +1,42 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, Text, TouchableOpacity } from "react-native";
+
+import styles from "./styles";
+
+import landingImg from "../../assets/images/landing.png";
+import studyIcon from "../../assets/images/icons/study.png";
+import giveClassesIcon from "../../assets/images/icons/give-classes.png";
+import heartIcon from "../../assets/images/icons/heart.png";
 
 function Landing() {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Image source={landingImg} style={styles.banner} />
+
+      <Text style={styles.title}>
+        Seja bem-vindo, {"\n"}
+        <Text style={styles.titleBold}>O que deseja fazer?</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+          <Image source={studyIcon} />
+
+          <Text style={styles.buttonText}>Estudar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+          <Image source={giveClassesIcon} />
+
+          <Text style={styles.buttonText}>Dar Aulas</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        Total de 200 conexões ja realisadas <Image source={heartIcon} />
+      </Text>
+    </View>
+  );
 }
 
 export default Landing;
