a1bfdb67a5bda00a01eb31780ac04194e14d3e4d
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-06 15:04:07 -0300

    add styles and navigator to PageHeader component

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,10 +1,37 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, Text } from "react-native";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+
+import backIcon from "../../assets/images/icons/back.png";
+import logoImg from "../../assets/images/logo.png";
 
 import styles from "./styles";
 
-function PageHeader() {
-  return <View />;
+interface PageHeaderProps {
+  title: string;
 }
+
+const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+  const { navigate } = useNavigation();
+
+  function handleGoBack() {
+    navigate("Landing");
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <BorderlessButton onPress={handleGoBack}>
+          <Image source={backIcon} resizeMode='contain' />
+        </BorderlessButton>
+
+        <Image source={logoImg} resizeMode='contain' />
+      </View>
+
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
 
 export default PageHeader;
