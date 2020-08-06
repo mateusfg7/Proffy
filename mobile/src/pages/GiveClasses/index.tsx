595918950fadb1b12c1f78759b7a9557842cbcea
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-06 13:34:22 -0300

    create GiveClasses page

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+// import { Container } from './styles';
+
+function GiveClasses() {
+  return <View />;
+}
+
+export default GiveClasses;
