11627d23cab09124f0786048c9830534615ab76f
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-06 13:36:45 -0300

    add styles to GiveClasses page

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { View } from "react-native";
 
-// import { Container } from './styles';
+import styles from "./styles";
 
 function GiveClasses() {
-  return <View />;
+  return <View style={styles.container} />;
 }
 
 export default GiveClasses;
---
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#8257e5",
+    justifyContent: "center",
+    padding: 40,
+  },
+});
+
+export default styles;
