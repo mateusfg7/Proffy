53c94e46ea9e1dca588cd7e5d5227a2940b1d131
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-06 14:12:47 -0300

    add handleNavigateToStudy on Landing page

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -17,6 +17,10 @@ function Landing() {
     navigate("GiveClasses");
   }
 
+  function handleNavigateToStudyPages() {
+    navigate("Study");
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -27,7 +31,10 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}
+        >
           <Image source={studyIcon} />
 
           <Text style={styles.buttonText}>Estudar</Text>
