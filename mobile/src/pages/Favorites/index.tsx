a4d6f80eff46e99bc4b6f9ff89679b2c42acae85
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-06 14:20:22 -0300

    add styles to favorites page

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { View } from "react-native";
 
+import styles from "./styles";
+
 function Favorites() {
-  return <View />;
+  return <View style={styles.container} />;
 }
 
 export default Favorites;
