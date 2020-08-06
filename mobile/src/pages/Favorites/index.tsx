8b40ab9688ae1300ab1b8e5c3d617feb7a053e9c
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-06 15:06:22 -0300

    use PageHeader component on TeacherList and Favorites page

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { View } from "react-native";
 
+import PageHeader from "../../components/PageHeader";
+
 import styles from "./styles";
 
 function Favorites() {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <PageHeader title='Meus Proffys Favoritos' />
+    </View>
+  );
 }
 
 export default Favorites;
