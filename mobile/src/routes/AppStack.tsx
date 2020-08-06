92bf65422ee9f552a45470b1b7810efc9866ba8b
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-06 13:34:54 -0300

    add GiveClassesPage on routerC

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Landing from "../pages/Landing";
+import GiveClasses from "../pages/GiveClasses";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -11,6 +12,7 @@ function AppStack() {
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name='Landing' component={Landing} />
+        <Screen name='GiveClasses' component={GiveClasses} />
       </Navigator>
     </NavigationContainer>
   );
