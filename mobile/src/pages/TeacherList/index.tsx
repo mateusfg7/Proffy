f470a5dbdb94c0a5d85c15b259dbc003799ec1c9
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-07 14:03:08 -0300

    add filter form to TeacherList page

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { View, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { View, ScrollView, Text, TextInput } from "react-native";
+import { BorderlessButton, RectButton } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
 
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
@@ -7,9 +9,57 @@ import TeacherItem from "../../components/TeacherItem";
 import styles from "./styles";
 
 function TeacherList() {
+  const [isFilterVisible, setIsFiltersVisble] = useState(false);
+
+  function handleToogleFiltersVisible() {
+    setIsFiltersVisble(!isFilterVisible);
+  }
+
   return (
     <View style={styles.container}>
-      <PageHeader title='Proffys Dsponíveis' />
+      <PageHeader
+        title='Proffys Dsponíveis'
+        headerRight={
+          <BorderlessButton onPress={handleToogleFiltersVisible}>
+            <Feather name='filter' size={20} color='#FFF' />
+          </BorderlessButton>
+        }
+      >
+        {isFilterVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput
+              placeholderTextColor='#c1bccc'
+              style={styles.input}
+              placeholder='Qual a matéria?'
+            />
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <TextInput
+                  placeholderTextColor='#c1bccc'
+                  style={styles.input}
+                  placeholder='Dia da semana'
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput
+                  placeholderTextColor='#c1bccc'
+                  style={styles.input}
+                  placeholder='Qual o horário'
+                />
+              </View>
+            </View>
+
+            <RectButton style={styles.submitButton}>
+              <Text style={styles.submitButtonText}>Filtrar</Text>
+            </RectButton>
+          </View>
+        )}
+      </PageHeader>
 
       <ScrollView
         style={styles.teacherList}
--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -9,6 +9,49 @@ const styles = StyleSheet.create({
   teacherList: {
     marginTop: -40,
   },
+
+  searchForm: {
+    marginBottom: 24,
+  },
+
+  label: {
+    color: "#d4c2ff",
+    fontFamily: "Poppins_400Regular",
+  },
+
+  input: {
+    height: 54,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    marginTop: 4,
+    marginBottom: 16,
+  },
+
+  inputGroup: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  inputBlock: {
+    width: "48%",
+  },
+
+  submitButton: {
+    backgroundColor: "#04d361",
+    height: 56,
+    borderRadius: 8,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  submitButtonText: {
+    color: "#fff",
+    fontFamily: "Archivo_700Bold",
+    fontSize: 16,
+  },
 });
 
 export default styles;
