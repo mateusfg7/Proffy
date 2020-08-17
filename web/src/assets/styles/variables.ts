55eeaae64a568d3f0dd48fe13ef7fa04fee72a01
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-17 10:13:54 -0300

    create style variables

---
+++ web/src/assets/styles/variables.ts
@@ -0,0 +1,22 @@
+const styleVariables = {
+  colorBackground: "#f0f0f7",
+  colorPrimaryLighter: "#9871f5",
+  colorPrimaryLight: "#916bea",
+  colorPrimary: "#8257e5",
+  colorPrimaryDark: "#774dd6",
+  colorPrimaryDarker: "#6842c2",
+  colorSecundary: "#04d361",
+  colorSecundaryDark: "#04bf58",
+  colorTitleInPrimary: "#ffffff",
+  colorTextInPrimary: "#d4c2ff",
+  colorTextTitle: "#32264d",
+  colorTextComplement: "#9c98a6",
+  colorTextBase: "#6a6180",
+  colorLineInWhite: "#e6e6f0",
+  colorInputBackground: "#f8f8fc",
+  colorButtonText: "#ffffff",
+  colorBoxBase: "#ffffff",
+  colorBoxFooter: "#fafafc",
+};
+
+export default styleVariables;
